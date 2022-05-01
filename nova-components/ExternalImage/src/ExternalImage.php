c147b4a12efc409ebdafe44e48d28db02613f869
Cameron M <cameronmurray@hotmail.ca>
2022-05-01 21:03:20 +0100

    Changed play to game for everything except route name

---
+++ nova-components/ExternalImage/src/ExternalImage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cmurray09\ExternalImage;
+
+use Laravel\Nova\Fields\Field;
+
+class ExternalImage extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'external-image';
+}
