af7bed72e6a288d04908fdc13967fd1d40c0297c
Cameron M <cameronmurray@hotmail.ca>
2022-05-02 22:45:22 +0100

    Data displayed correctly instead of json

--- resources/js/Pages/Play.vue
+++ resources/js/Pages/Play.vue
@@ -1,7 +1,39 @@
 <template>
     <section>
-        <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
-            <div><h3>{{ games }}</h3></div>
+        <div id="darkMode">
+            <div class="row expanded columns" id="mainSearchPage">
+                <h3 class="gameTitle">Wired</h3>
+                <div class="arrowContainer">
+                    <div class="leftArrow">
+                        <i class="fa fa-angle-left"></i>
+                    </div>
+                    <div class="rightArrow">
+                        <i class="fa fa-angle-right"></i>
+                    </div>
+                    <div class="sliderContainer">
+                        <div class="leftBlocker"></div>
+                        <div class="rightBlocker"></div>
+
+                        <div class="sliderMech">
+                            <div class="sliderContent" v-for="game in games">
+                                <div class="sliderContentChild">
+                                    <div class="gameImageContainer">
+                                        <img class="gameThumbnail" v-bind:src="game.thumbnail">
+                                        <p class="hideData">{{ game.id }}</p>
+                                        <p class="hideData gameOwner">{{ game.owner }}</p>
+                                        <p class="hideData room_link">{{ game.room_link }}</p>
+                                        <p class="hideData">{{ game.co_owner }}</p>
+                                        <p class="hideData gameRoomCount">{{ game.room_count }}</p>
+                                        <p class="hideData gameCategory">{{ game.category }}</p>
+                                        <p class="hideData gameType">{{ game.game_type }}</p>
+                                        <p class="hideData gameName">{{ game.name }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -11,7 +43,19 @@ import {defineComponent} from 'vue'
 
 export default defineComponent({
     components: {},
-    props: ['games', 'nonwiredGame'],
+    props: {
+        games: {
+            thumbnail: String,
+            id: Number,
+            owner: String,
+            room_link: String,
+            co_owner: String,
+            room_count: Number,
+            category: String,
+            game_type: String,
+            name: String,
+        }
+    },
     mounted: function () {
         this.$nextTick(function () {
             // $('.multiple-items').slick({
