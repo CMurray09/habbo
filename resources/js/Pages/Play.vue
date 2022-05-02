4e30c682727f41bd0615254382f0a942d9d652c3
Cameron M <cameronmurray@hotmail.ca>
2022-05-02 17:48:58 +0100

    Sending data to view

--- resources/js/Pages/Play.vue
+++ resources/js/Pages/Play.vue
@@ -1,6 +1,8 @@
 <template>
     <section>
-        <h1>Play Page</h1>
+        <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}'>
+            <div><h3>{{ games }}</h3></div>
+        </div>
     </section>
 </template>
 
@@ -9,5 +11,16 @@ import {defineComponent} from 'vue'
 
 export default defineComponent({
     components: {},
+    props: ['games', 'nonwiredGame'],
+    mounted: function () {
+        this.$nextTick(function () {
+            // $('.multiple-items').slick({
+            //     infinite: true,
+            //     slidesToShow: 3,
+            //     slidesToScroll: 3
+            // });
+        })
+    }
+
 })
 </script>
