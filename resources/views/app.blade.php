fbc85dbfb3a7bbb710b6ab6a6649fdc508ef81f8
Cameron M <cameronmurray@hotmail.ca>
2022-05-26 00:08:33 +0100

    jquery, gsap, fitty, foundation cdn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,10 +12,12 @@
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.7.4/css/foundation-float.min.css">
 
     <!-- Scripts -->
     @routes
     <script src="{{ mix('js/app.js') }}" defer></script>
+
 </head>
 <body class="font-sans antialiased">
 @inertia
@@ -23,5 +25,9 @@
 @env ('local')
 <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
 @endenv
+<script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fitty/2.2.6/fitty.min.js"></script>
 </body>
 </html>
